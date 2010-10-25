e9faf720598f7a1799cf033c863001a205db4aec
jono <jono@f37d451b-ab52-48fe-b4e5-9fc9f3fe60d4>
2010-10-25 07:24:12 +0000

    Added in my old grid menu code; will use it for tools menu
    git-svn-id: file:///var/svn/multicanvas@13 f37d451b-ab52-48fe-b4e5-9fc9f3fe60d4

---
+++ menu.js
@@ -0,0 +1,212 @@
+function GridMenu( canvas, squareSize, stringList, isSubMenu ) {
+    this._left = 0;
+    this._top = 0;
+    this._timeDelay = 250; //ms -- if mouse released before this time elapsed, stay open and wait for a 
+    //second click.
+    this._squareSize = squareSize;
+    //this._optionsList = optionsList;
+    this._visible = false;
+    this._ctx = canvas.getContext("2d");
+    this._lastChosen = null;
+    this._oldCell = null;
+    this._invokeTime = 0;
+
+    if (isSubMenu) {
+	this._isSubMenu = true;
+    } else {
+	this._isSubMenu = false;
+    }
+
+    if (stringList.length > 8 ) {
+	this._stringList = stringList.slice(0, 7);
+	this._stringList.push( ["Search...", null] );
+	this._subMenu = new GridMenu( canvas, squareSize, stringList.slice( 7 ), true );
+    } else {
+	this._stringList = stringList;
+	this._subMenu = null;
+    }
+
+    this._commands = [];
+    for( var i = 0; i < this._stringList.length; i++ ) {
+	this._commands.push( { name: this._stringList[i][0], icon: this._stringList[i][1]} );
+    }
+}
+GridMenu.prototype = {
+    onMouseDown: function(x, y) {
+	if (this._visible) {
+	    return;
+	}
+	this._invokeTime = new Date().getTime();
+	this._left = x - this._squareSize * 1.5;
+	this._top = y - this._squareSize * 1.5;
+	if (this._left < 0) {
+	    this._left = 0;
+	}
+	if (this._top < 0) {
+	    this._top = 0;
+	}
+	if (this._left > 600 - this._squareSize * 3 ) {
+	    this._left = 600 - this._squareSize * 3;
+	    if (this._isSubMenu) {
+		this._top += this._squareSize;
+	    }
+	}
+	if (this._top > 600 - this._squareSize * 3 ) {
+	    this._top = 600 - this._squareSize * 3;
+	    if (this._isSubMenu) {
+		this._left += this._squareSize;
+	    }
+	}
+	this._draw();
+	this._visible = true;
+    },
+    onMouseUp: function(x, y) {
+	if (this._visible) {
+	    var now = new Date().getTime();
+	    if ( now - this._invokeTime > this._timeDelay ) {
+		redrawCanvas(this._ctx);
+		this._lastChosen = this._getCellNumFromPoint( x, y );
+		this._visible = false;
+	    }
+	}
+    },
+    onMouseMove: function(x, y) {
+	if (!this._visible) {
+	    return;
+	}
+	var cellNum = this._getCellNumFromPoint(x, y);
+	if (cellNum >= 8 ) {
+	    return this._subMenu.onMouseMove(x, y);
+	}
+
+	if (this._subMenu) {
+	    if (cellNum == 7 && !this._subMenu._visible) {
+		this._subMenu.onMouseDown(this._left + 3.5 * this._squareSize, this._top + 3.5 * this._squareSize);
+	    } else if (cellNum < 7 && this._subMenu._visible) {
+		this._subMenu.onMouseUp(this._left + 3.5 * this._squareSize, this._top + 3.5 * this._squareSize);
+		this._draw();
+	    }
+	}
+	if (cellNum != this._oldCell) {
+	    if (this._oldCell != null)
+		this._fillSquare(this._oldCell, "white");
+	    if (cellNum != null)
+		this._fillSquare(cellNum, "red");
+	    this._oldCell = cellNum;
+	}
+    },
+    _getCellNumFromPoint: function( x, y ) {
+	var col = Math.floor( ( x - this._left ) / this._squareSize );
+	var row = Math.floor( ( y - this._top ) / this._squareSize );
+	if ( this._subMenu && this._subMenu._visible && (col > 2 || row > 2) ) {
+	    return (this._subMenu._getCellNumFromPoint(x, y) + 8 );
+	}
+	if (col < 0) col = 0;
+	if (col > 2) col = 2;
+	if (row < 0) row = 0;
+	if (row > 2) row = 2;
+	return this._colRowToCellNum(col, row);
+    },
+
+    getLastChosen: function() {
+	return this._lastChosen;
+    },
+
+    _draw: function() {
+	this._ctx.moveTo( this._left, this._top );
+	this._ctx.lineTo( this._left + 3 * this._squareSize, this._top );
+	this._ctx.lineTo( this._left + 3 * this._squareSize, this._top + 3 * this._squareSize );
+	this._ctx.lineTo( this._left, this._top + 3 * this._squareSize );
+	this._ctx.lineTo( this._left, this._top );
+	this._ctx.stroke();
+
+	this._ctx.moveTo( this._left + this._squareSize, this._top );
+	this._ctx.lineTo( this._left + this._squareSize, this._top + 3 * this._squareSize );
+	this._ctx.stroke();
+
+	this._ctx.moveTo( this._left + this._squareSize * 2, this._top );
+	this._ctx.lineTo( this._left + this._squareSize * 2, this._top + 3 * this._squareSize );
+	this._ctx.stroke();
+
+	this._ctx.moveTo( this._left, this._top + this._squareSize);
+	this._ctx.lineTo( this._left + this._squareSize * 3, this._top +  this._squareSize );
+	this._ctx.stroke();
+
+	this._ctx.moveTo( this._left, this._top + this._squareSize * 2);
+	this._ctx.lineTo( this._left + this._squareSize * 3, this._top +  this._squareSize * 2);
+	this._ctx.stroke();
+
+	for (var i = 0; i < 8; i++) {
+	    if ( i < this._commands.length )
+		this._renderTextInSquare(this._commands[i], i);
+	}
+    },
+
+    _cellNumToColRow: function(cellNum) {
+	var table;
+	if (this._isSubMenu) {
+	    table = [[1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]];
+	} else {
+	    table = [[0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [0, 2], [1, 2], [2, 2]];
+	} 
+	return table[cellNum];
+    },
+
+    _colRowToCellNum: function(col, row) {
+	var table;
+	if (this._isSubMenu) {
+	    table = [[1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]];
+	} else {
+	    table = [[0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [0, 2], [1, 2], [2, 2]];
+	} 
+	for ( var i = 0; i < 8; i++ ) {
+	    if (table[i][0] == col && table[i][1] == row)
+		return i;
+	}
+	return null;
+    },
+
+    _renderTextInSquare: function(commandObj, cellNum) {
+	this._ctx.mozTextStyle = "12pt sans serif";
+	this._ctx.fillStyle = "black";
+	this._ctx.save();
+	var colRow = this._cellNumToColRow(cellNum);
+	var col = colRow[0];
+	var row = colRow[1];
+	var margin = 5; // five pixels
+	this._ctx.translate(this._left + this._squareSize * col + margin, this._top + this._squareSize * (row + 1) - margin );
+	this._ctx.mozDrawText(commandObj.name);
+	this._ctx.restore();
+
+	if (commandObj.icon) {
+	    var img = new Image();
+	    var ctx= this._ctx;
+	    var x = this._left + this._squareSize * col + margin;
+	    var y = this._top + this._squareSize * row + margin;
+	    img.onload = function(){ ctx.drawImage(img, x, y);  };
+	    img.src = commandObj.icon;
+	    /*this._ctx.save();
+      this._ctx.translate(this._left + this._squareSize * col + margin, this._top + this._squareSize * (row) + 12 + margin );
+      this._ctx.mozDrawText(commandObj.icon);
+      this._ctx.restore();*/
+	}
+    },
+
+    _fillSquare: function( cellNum, color ) {
+	this._ctx.fillStyle = color;
+	var colRow = this._cellNumToColRow(cellNum);
+	var col = colRow[0];
+	var row = colRow[1];
+	this._ctx.beginPath();
+	this._ctx.moveTo(this._left + this._squareSize * col, this._top + this._squareSize * row );
+	this._ctx.lineTo(this._left + this._squareSize * col, this._top + this._squareSize * (row + 1) );
+	this._ctx.lineTo(this._left + this._squareSize * (col + 1), this._top + this._squareSize * (row + 1) );
+	this._ctx.lineTo(this._left + this._squareSize * (col + 1), this._top + this._squareSize * row );
+	this._ctx.lineTo(this._left + this._squareSize * col, this._top + this._squareSize * row );
+	this._ctx.fill();
+	this._ctx.fillStyle = "black";
+	this._ctx.stroke();
+	if (cellNum < this._commands.length )
+	    this._renderTextInSquare(this._commands[cellNum], cellNum);
+    }
+};
