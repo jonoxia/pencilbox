d66514723f5e248286053ad2bd37f3d9a7528fc4
jono <jono@f37d451b-ab52-48fe-b4e5-9fc9f3fe60d4>
2010-12-11 19:34:37 +0000

    Added license blocks to all js files
    git-svn-id: file:///var/svn/multicanvas@86 f37d451b-ab52-48fe-b4e5-9fc9f3fe60d4

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Pencilbox.
+ *
+ * The Initial Developer of the Original Code is Jono Xia.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Jono X <jono@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 var g_drawInterface = null;
 var g_toolInterface = null;
 var g_history = null;
@@ -97,6 +133,7 @@ function adjustToScreen() {
 	$("#pen-size-canvas").attr("width", screenWidth * 0.2);
 	$("#pen-size-canvas").attr("height", screenHeight * 0.7);
     }
+
     $("#the-canvas").attr("width", mainCanvasWidth);
     $("#the-canvas").attr("height", mainCanvasHeight);
 
@@ -166,7 +203,6 @@ $(function() {
 	    // (skip this if there are already drawing layers due to 
 	    // recreated history)
 	    if (g_drawInterface.layers.length < 4) {
-		$("#debug").html("Only " + g_drawInterface.layers.length + " layers");
 		g_drawInterface.newLayer();
 	    }
 	    g_drawInterface.updateAllLayerDisplays();
