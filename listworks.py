93f53521ac8c3be0feef22716c407a20fa8f69a7
Jono X <ebjono@gmail.com>
2011-08-26 05:02:52 +0000

    Bug 6 - now using external html templates for page generation instead of inlining all the html.  Added delete function for each saved image.

--- listworks.py
+++ listworks.py
@@ -4,23 +4,54 @@
 # works drawn by that user.
 from savedHistoryTable import DrawingHistory
 
+TEMPLATE_DIR = "/var/www/pencilbox2"
+
+import os
 import cgi
 import cgitb
+import string
 
-cgitb.enable()
-q = cgi.FieldStorage()
-artist = q.getfirst("artist", "")
+def render_template_file( filename, substitutionDict ):
+    file = open( os.path.join( TEMPLATE_DIR, filename ), "r")
+    template = string.Template(file.read())
+    file.close()
+    return template.substitute( substitutionDict )
 
 
-matches = DrawingHistory.selectBy(owner = artist)
-print "Content-type: text/html"
-print
+def printList(artist):
+    print "Content-type: text/html"
+    print
 
-if matches.count() > 0:
-    print "<html><head><title>Pencilbox - Works By %s</title>" % artist
-    print "</head><body><table>"
-    for match in matches:
-        title = match.title
-        url = "touchscreen.html?filename=%s" % title
-        print "<tr><td><a href=\"%s\">%s</a></td></tr>" % (url, title)
-    print "</table></body></html>"
+    matches = DrawingHistory.selectBy(owner = artist)
+    if matches.count() > 0:
+        work_list = ""
+        for match in matches:
+            title = match.title
+            url = "touchscreen.html?filename=%s" % title
+            work_list += render_template_file( "listwork_row.html",
+                                               {"url": url,
+                                                "title": title,
+                                                "artist": artist} )
+    
+        print render_template_file( "listworks.html", {"artist": artist,
+                                                       "work_list": work_list})
+    else:
+        print "No matches for %s" % artist
+
+
+if __name__ == "__main__":
+    cgitb.enable()
+    q = cgi.FieldStorage()
+
+    action = q.getfirst("action", "")
+    artist = q.getfirst("artist", "")
+    title = q.getfirst("title", "")
+
+    if action == "del":
+        matches = DrawingHistory.selectBy(owner = artist, title = title)
+        if matches.count() > 0:
+            DrawingHistory.delete(matches[0].id)
+
+    printList(artist)
+
+
